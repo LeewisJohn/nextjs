bfeb8324a6ce85350e448c7aec434106e49c36bc
LeewisJohn <john.lk.den@gmail.com>
2024-11-03 20:42:30 +0700

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,16 +10,30 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true, // Allow credentials if needed
+  });
+
+  // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Set global prefix for all routes
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Start the application
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
